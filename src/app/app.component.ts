f587fea80b2ef90226105792842b00414d5de1b3
Hossain Kabir <sweat449@gmail.com>
2021-12-27 17:16:00 +0600

    drag-drop done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DragDropPractice';
+
+  weeks: any[] = [];
+  connectedTo: any = [];
+
+  constructor() {
+    this.weeks = [
+      {
+        id: 'Animal',
+        weeklist: [
+          "item 1",
+          "item 2",
+          "item 3",
+          "item 4",
+          "item 5"
+        ]
+      }, {
+        id: 'Fruits',
+        weeklist: [
+          "item 1",
+          "item 2",
+          "item 3",
+          "item 4",
+          "item 5"
+        ]
+      }, {
+        id: 'Vagetable',
+        weeklist: [
+          "item 1",
+          "item 2",
+          "item 3",
+          "item 4",
+          "item 5"
+        ]
+      }, {
+        id: 'Human',
+        weeklist: [
+          "item 1",
+          "item 2",
+          "item 3",
+          "item 4",
+          "item 5"
+        ]
+      },
+    ];
+    for (let week of this.weeks) {
+      this.connectedTo.push(week.id);
+    };
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
+  dropCategory(event: CdkDragDrop<any[]>) {
+    console.log(event);
+    moveItemInArray(this.weeks, event.previousIndex, event.currentIndex);
+  }
+
 }
